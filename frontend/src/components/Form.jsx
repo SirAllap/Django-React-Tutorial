83ea0950fedd77caf0ecca452775e6619346423b
SirAllap <serallapdivad@gmail.com>
2024-08-30 17:50:49 +0200

    improved form form
    Now user will login with email
    Now uer will have to provide an email to register

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,62 +1,97 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import { useState } from 'react'
-import api from '../api'
-import { useNavigate } from 'react-router-dom'
-import { ACCESS_TOKEN, REFRESH_TOKEN } from '../constants'
-import '../styles/Form.css'
+import { useState } from "react";
+import api from "../api";
+import { useNavigate } from "react-router-dom";
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
+import "../styles/Form.css";
 
 const Form = ({ route, method }) => {
-	const [username, setUsername] = useState('')
-	const [password, setPassword] = useState('')
-	const [loading, setLoading] = useState(false)
-	const navigate = useNavigate()
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
-	const name = method === 'login' ? 'Login' : 'Register'
+  const name = method === "login" ? "Login" : "Register";
 
-	const handleSubmit = async (e) => {
-		setLoading(true)
-		e.preventDefault()
+  const handleSubmit = async (e) => {
+    setLoading(true);
+    e.preventDefault();
+    const data =
+      method === "login" ? { email, password } : { username, email, password };
 
-		try {
-			const res = await api.post(route, { username, password })
-			if (method === 'login') {
-				localStorage.setItem(ACCESS_TOKEN, res.data.access)
-				localStorage.setItem(REFRESH_TOKEN, res.data.refresh)
-				navigate('/')
-			} else {
-				navigate('/login')
-			}
-			navigate('/')
-		} catch (error) {
-			alert(error)
-		} finally {
-			setLoading(false)
-		}
-	}
+    try {
+      const res = await api.post(route, data);
+      if (method === "login") {
+        localStorage.setItem(ACCESS_TOKEN, res.data.access);
+        localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
+        navigate("/");
+      } else {
+        navigate("/login");
+      }
+      navigate("/");
+    } catch (error) {
+      alert(error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-	return (
-		<form onSubmit={handleSubmit} className='form-container'>
-			<h1>{name}</h1>
-			<input
-				className='form-input'
-				type='text'
-				value={username}
-				onChange={(e) => setUsername(e.target.value)}
-				placeholder='Username'
-			/>
-			<input
-				className='form-input'
-				type='password'
-				value={password}
-				onChange={(e) => setPassword(e.target.value)}
-				placeholder='Password'
-			/>
-			<button className='form-button' type='submit'>
-				{name}
-			</button>
-		</form>
-	)
-}
+  return (
+    <form onSubmit={handleSubmit} className="form-container">
+      <h1>{name}</h1>
+      <input
+        className="my-2 ml-2 px-2 py-2 w-full border border-indigo-300/40 bg-white rounded-md text-indigo-900"
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Email"
+      />
+      {method !== "login" && (
+        <input
+          className="my-2 ml-2 px-2 py-2 w-full border border-indigo-300/40 bg-white rounded-md text-indigo-900"
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="Username"
+        />
+      )}
 
-export default Form
+      <input
+        className="my-2 ml-2 px-2 py-2 w-full border border-indigo-300/40 bg-white rounded-md text-indigo-900"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+      />
+
+      <p className="my-2">
+        {method === "login" ? "Haven't register yet? " : "Already register? "}
+        {method === "login" ? (
+          <span
+            className="text-indigo-500 font-bold cursor-pointer hover:text-indigo-600"
+            onClick={() => navigate("/register")}
+          >
+            Register
+          </span>
+        ) : (
+          <span
+            className="text-indigo-500 font-bold cursor-pointer hover:text-indigo-600"
+            onClick={() => navigate("/login")}
+          >
+            Login
+          </span>
+        )}
+      </p>
+      <button
+        className="border border-indigo-300 bg-indigo-500 py-2 px-5 min-w-full text-white rounded-md hover:bg-transparent hover:text-indigo-500 transition-all duration-300"
+        type="submit"
+      >
+        {loading ? "Loading..." : name}
+      </button>
+    </form>
+  );
+};
+
+export default Form;
