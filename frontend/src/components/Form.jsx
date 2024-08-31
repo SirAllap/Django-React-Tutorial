b32893a19ef3218d80a756b773e5e3cccda17c9a
SirAllap <serallapdivad@gmail.com>
2024-08-31 19:12:17 +0200

    frontend with expenses view working

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -11,42 +11,86 @@ const Form = ({ route, method }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
+  const [emailError, setEmailError] = useState(null);
+  const [passwordError, setPasswordError] = useState(null);
   const navigate = useNavigate();
 
+  // Determine if the form is for login or registration
   const name = method === "login" ? "Login" : "Register";
 
   const handleSubmit = async (e) => {
-    setLoading(true);
     e.preventDefault();
+    // Basic form validation
+    if (!email || !password || (method !== "login" && !username)) {
+      alert("Please fill in all required fields.");
+      return;
+    }
+
+    setLoading(true);
+
+    // Prepare data payload based on the method
     const data =
       method === "login" ? { email, password } : { username, email, password };
 
     try {
+      // Send data to the specified API route
       const res = await api.post(route, data);
+
       if (method === "login") {
+        // Save tokens if the login is successful
         localStorage.setItem(ACCESS_TOKEN, res.data.access);
         localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
-        navigate("/");
+        navigate("/"); // Redirect to home
       } else {
+        // Redirect to login after registration
         navigate("/login");
       }
-      navigate("/");
     } catch (error) {
-      alert(error);
+      // Display the error message to the user
+      // alert("An error occurred. Please check your input and try again.");
+      console.error("Error:", error);
+      if (error.response.data.email)
+        setEmailError(error.response.data.email[0]);
+      if (error.response.data.password)
+        setPasswordError(error.response.data.password[0]);
     } finally {
-      setLoading(false);
+      setLoading(false); // Reset loading state
     }
   };
 
   return (
     <form onSubmit={handleSubmit} className="form-container">
       <h1>{name}</h1>
+      {emailError && (
+        <div
+          className="bg-red-100 border border-red-400 text-red-700 my-2 ml-2 px-2 py-2 rounded-md relative w-full"
+          role="alert"
+        >
+          <strong className="font-bold mr-10">{emailError}</strong>
+          <span
+            className="absolute top-0 bottom-0 right-0 px-2 py-2"
+            onClick={() => setEmailError(null)}
+          >
+            <svg
+              className="fill-current h-6 w-6 text-red-500"
+              role="button"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <title>Close</title>
+              <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+            </svg>
+          </span>
+        </div>
+      )}
+
       <input
-        className="my-2 ml-2 px-2 py-2 w-full border border-indigo-300/40 bg-white rounded-md text-indigo-900"
+        className="my-2 ml-2 px-2 py-2 w-full border border-indigo-300/40 bg-white rounded-md text-indigo-900 error"
         type="email"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
         placeholder="Email"
+        required
       />
       {method !== "login" && (
         <input
@@ -55,19 +99,45 @@ const Form = ({ route, method }) => {
           value={username}
           onChange={(e) => setUsername(e.target.value)}
           placeholder="Username"
+          required
         />
       )}
 
+      {passwordError && (
+        <div
+          className="bg-red-100 border border-red-400 text-red-700 my-2 ml-2 px-2 py-2 rounded-md relative w-full"
+          role="alert"
+        >
+          <strong className="font-bold mr-10">{passwordError}</strong>
+          <span
+            className="absolute top-0 bottom-0 right-0 px-2 py-2"
+            onClick={() => setPasswordError(null)}
+          >
+            <svg
+              className="fill-current h-6 w-6 text-red-500"
+              role="button"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <title>Close</title>
+              <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+            </svg>
+          </span>
+        </div>
+      )}
       <input
         className="my-2 ml-2 px-2 py-2 w-full border border-indigo-300/40 bg-white rounded-md text-indigo-900"
         type="password"
         value={password}
         onChange={(e) => setPassword(e.target.value)}
         placeholder="Password"
+        required
       />
 
       <p className="my-2">
-        {method === "login" ? "Haven't register yet? " : "Already register? "}
+        {method === "login"
+          ? "Haven't registered yet? "
+          : "Already registered? "}
         {method === "login" ? (
           <span
             className="text-indigo-500 font-bold cursor-pointer hover:text-indigo-600"
@@ -87,6 +157,7 @@ const Form = ({ route, method }) => {
       <button
         className="border border-indigo-300 bg-indigo-500 py-2 px-5 min-w-full text-white rounded-md hover:bg-transparent hover:text-indigo-500 transition-all duration-300"
         type="submit"
+        disabled={loading}
       >
         {loading ? "Loading..." : name}
       </button>
--- frontend/src/pages/Expenses.jsx
+++ frontend/src/pages/Expenses.jsx
@@ -1,91 +1,212 @@
-import { useState, useEffect } from 'react'
-import api from "../api"
-import { ToggleScreenMode } from "../components/ToggleScreenMode"
-import { LogoutButton } from "../components/LogoutButton"
-import { useNavigate } from "react-router-dom"
+import { useState, useEffect } from "react";
+import api from "../api";
+import { ToggleScreenMode } from "../components/ToggleScreenMode";
+import { LogoutButton } from "../components/LogoutButton";
+import { useNavigate } from "react-router-dom";
 
 const Expenses = () => {
-    const [expenses, setExpenses] = useState([])
-    const [newAmount, setNewAmount] = useState("")
-    const [newContent, setNewContent] = useState("")
-    const navigate = useNavigate()
+  const [expenses, setExpenses] = useState([]);
+  const [editId, setEditId] = useState(null);
+  const [editedAmount, setEditedAmount] = useState("");
+  const [editedContent, setEditedContent] = useState("");
+  const [newAmount, setNewAmount] = useState("");
+  const [newContent, setNewContent] = useState("");
+  const navigate = useNavigate();
 
-    // useEffect(() => {
-    //     getExpenses()
-    // }, [])
+  useEffect(() => {
+    getExpenses();
+  }, []);
 
-    const getExpenses = () => {
-        api
-            .get("/api/expenses/")
-            .then((res) => res.data)
-            .then((data) => {
-                setExpenses(data)
-            })
-            .catch((err) => alert(err))
-    }
+  const getExpenses = () => {
+    api
+      .get("/api/expenses/")
+      .then((res) => res.data)
+      .then((data) => {
+        setExpenses(data);
+      })
+      .catch((err) => alert(err));
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        api
-            .post("/api/expenses/", { amount: newAmount, content: newContent })
-            .then((res) => {
-                if (res.status === 201) alert("Expense created")
-                else alert("Failed to create expense")
-                getExpenses()
-                setNewAmount("")
-                setNewContent("")
-            })
-            .catch((err) => alert(err))
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    api
+      .post("/api/expenses/", { amount: newAmount, content: newContent })
+      .then((res) => {
+        if (res.status === 201) alert("Expense created");
+        else alert("Failed to create expense");
+        getExpenses();
+        setNewAmount("");
+        setNewContent("");
+      })
+      .catch((err) => alert(err));
+  };
 
-    console.log(expenses)
+  const deleteExpense = (id) => {
+    api
+      .delete(`/api/expenses/delete/${id}/`)
+      .then((res) => {
+        if (res.status === 204) alert("Expenses deleted");
+        else alert("Failed to delete expense");
+        getExpenses();
+      })
+      .catch((err) => alert(err));
+  };
 
-    return (
-        <div className="max-w-screen-xl mx-auto py-5 px-5">
-            <section className="max-w-screen-xl flex flex-row justify-end gap-5">
-                <button className='border border-yellow-300 bg-yellow-500 py-2 px-5 text-white rounded-md hover:bg-transparent hover:text-yellow-500 transition-all duration-300' onClick={() => navigate("/")}>
-                    Add note
-                </button>
-                <ToggleScreenMode />
-                <LogoutButton />
-            </section>
-            <div className="flex flex-col justify-between items-center my-5">
-                <h2 className="text-3xl">Expenses</h2>
-            </div>
-            <form
-                onSubmit={handleSubmit}
-                className="max-w-screen-sm mx-auto flex flex-col gap-3"
-            >
-                <label htmlFor="amount">Amount:</label>
-                <input
-                    className="py-2 px-2 border border-indigo-300 bg-indigo-100 rounded-md dark:text-gray-800"
+  const startEditExpense = (expense) => {
+    setEditId(expense.id);
+    setEditedAmount(expense.amount);
+    setEditedContent(expense.content);
+  };
+
+  const applyEditExpense = (id) => {
+    api
+      .put(`/api/expenses/update/${id}/`, {
+        amount: editedAmount,
+        content: editedContent,
+      })
+      .then((res) => {
+        if (res.status === 200) alert("Expense updated");
+        else alert("Failed to update expense");
+        getExpenses();
+        setEditId(null);
+      })
+      .catch((err) => alert(err));
+  };
+
+  const cancelEdit = () => {
+    setEditId(null);
+  };
+
+  return (
+    <div className="max-w-screen-xl mx-auto py-5 px-5">
+      <section className="max-w-screen-xl flex flex-row justify-end gap-5">
+        <button
+          className="border border-yellow-300 bg-yellow-500 py-2 px-5 text-white rounded-md hover:bg-transparent hover:text-yellow-500 transition-all duration-300"
+          onClick={() => navigate("/")}
+        >
+          Add notes
+        </button>
+        <ToggleScreenMode />
+        <LogoutButton />
+      </section>
+      <div className="flex flex-col justify-between items-center my-5">
+        <h2 className="text-3xl">Expenses</h2>
+      </div>
+      <form
+        onSubmit={handleSubmit}
+        className="max-w-screen-sm mx-auto flex flex-col gap-3"
+      >
+        <label htmlFor="amount">Amount:</label>
+        <input
+          className="py-2 px-2 border border-indigo-300 bg-indigo-100 rounded-md dark:text-gray-800"
+          type="text"
+          id="amount"
+          name="amount"
+          required
+          onChange={(e) => setNewAmount(e.target.value)}
+          value={newAmount}
+        />
+
+        <label htmlFor="content">Content:</label>
+        <textarea
+          className="py-2 px-2 border border-indigo-300 bg-indigo-100 rounded-md dark:text-gray-800"
+          id="content"
+          name="content"
+          required
+          onChange={(e) => setNewContent(e.target.value)}
+          value={newContent}
+        />
+
+        <input
+          className="border border-indigo-300 bg-indigo-500 py-2 px-5 text-white rounded-md hover:bg-transparent hover:text-indigo-500 transition-all duration-300"
+          type="submit"
+          value="Submit"
+        />
+      </form>
+
+      <hr className="my-5" />
+      <div className="flex flex-col gap-10 max-w-screen-sm mx-auto">
+        {expenses.map((expense) => (
+          <div
+            key={expense.id}
+            className="border-2 border-dotted border-indigo-400 rounded-lg py-5 px-3 flex flex-col gap-5"
+          >
+            <section className="max-w-xl mx-auto flex flex-col gap-2">
+              <section className="flex flex-row align-middle justify-between">
+                <h3 className="text-xl font-bold">Amount:</h3>
+                {editId !== expense.id ? (
+                  <span className="py-1 px-2 font-normal w-full capitalize-first">
+                    {expense.amount} €
+                  </span>
+                ) : (
+                  <input
+                    className="ml-2 py-1 px-2 w-full border border-indigo-300/40 bg-transparent rounded-md"
                     type="text"
-                    id="amount"
-                    name="amount"
+                    id="title"
+                    name="title"
                     required
-                    onChange={(e) => setNewAmount(e.target.value)}
-                    value={newAmount}
-                />
-
-                <label htmlFor="content">Content:</label>
-                <textarea
-                    className="py-2 px-2 border border-indigo-300 bg-indigo-100 rounded-md dark:text-gray-800"
+                    onChange={(e) => setEditedAmount(e.target.value)}
+                    value={editedAmount}
+                  />
+                )}
+              </section>
+              <section className="flex flex-row align-middle justify-between">
+                <p className="text-xl font-bold">Content:</p>
+                {editId !== expense.id ? (
+                  <span className="py-1 px-2 font-normal w-full capitalize-first">
+                    {expense.content}
+                  </span>
+                ) : (
+                  <textarea
+                    className="ml-2 py-1 px-2 w-full border border-indigo-300/40 bg-transparent rounded-md"
                     id="content"
                     name="content"
                     required
-                    onChange={(e) => setNewContent(e.target.value)}
-                    value={newContent}
-                />
+                    onChange={(e) => setEditedContent(e.target.value)}
+                    value={editedContent}
+                  />
+                )}
+              </section>
+            </section>
+            <section>Author of the expense: {expense.author.username}</section>
+            <section className="w-full flex align-middle justify-end gap-2">
+              {editId !== expense.id ? (
+                <>
+                  <button
+                    className="border border-indigo-300 bg-indigo-500 py-2 px-5 min-w-32 text-white rounded-md hover:bg-transparent hover:text-indigo-500 transition-all duration-300"
+                    onClick={() => startEditExpense(expense)}
+                  >
+                    Edit
+                  </button>
+                  <button
+                    className="border border-red-300 bg-red-500 py-2 px-5 min-w-32 text-white rounded-md hover:bg-transparent hover:text-red-500 transition-all duration-300"
+                    onClick={() => deleteExpense(expense.id)}
+                  >
+                    Delete
+                  </button>
+                </>
+              ) : (
+                <>
+                  <button
+                    className="border border-green-300 bg-green-500 py-2 px-5 min-w-32 text-white rounded-md hover:bg-transparent hover:text-green-500 transition-all duration-300"
+                    onClick={() => applyEditExpense(expense.id)}
+                  >
+                    Apply
+                  </button>
+                  <button
+                    className="border border-yellow-300 bg-yellow-500 py-2 px-5 min-w-20 text-white rounded-md hover:bg-transparent hover:text-yellow-500 transition-all duration-300"
+                    onClick={cancelEdit}
+                  >
+                    Cancel
+                  </button>
+                </>
+              )}
+            </section>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-                <input
-                    className="border border-indigo-300 bg-indigo-500 py-2 px-5 text-white rounded-md hover:bg-transparent hover:text-indigo-500 transition-all duration-300"
-                    type="submit"
-                    value="Submit"
-                />
-            </form>
-        </div>
-    )
-}
-
-
-export default Expenses
+export default Expenses;
