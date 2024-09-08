218821c0d01869060ec2fee72b811918fa9ab187
SirAllap <serallapdivad@gmail.com>
2024-09-08 15:17:01 +0200

    added useState to controll the name of the current theme

--- frontend/src/components/ToggleScreenMode.jsx
+++ frontend/src/components/ToggleScreenMode.jsx
@@ -1,31 +1,37 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from "react";
 
 export const ToggleScreenMode = () => {
-    const handleToggleMode = () => {
-        console.log("clicked")
-        const currentTheme = localStorage.getItem("theme")
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const handleToggleMode = () => {
+    console.log("clicked");
+    const currentTheme = localStorage.getItem("theme");
 
-        if (currentTheme === "dark") {
-            document.documentElement.classList.remove("dark")
-            localStorage.setItem("theme", "light")
-        } else {
-            document.documentElement.classList.add("dark")
-            localStorage.setItem("theme", "dark")
-        }
+    if (currentTheme === "dark") {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+      setIsDarkMode(true);
+    } else {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+      setIsDarkMode(false);
     }
+  };
 
-    useEffect(() => {
-        const savedTheme = localStorage.getItem("theme") || "light"
-        if (savedTheme === "dark") {
-            document.documentElement.classList.add("dark")
-        } else {
-            document.documentElement.classList.remove("dark")
-        }
-    }, [])
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme") || "light";
+    if (savedTheme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, []);
 
-    return (
-        <button className='border border-indigo-300 bg-indigo-500 py-2 px-5 text-white rounded-md hover:bg-transparent hover:text-indigo-500 transition-all duration-300' onClick={handleToggleMode}>
-            Change Mode (Dark/Light)
-        </button>
-    )
-}
+  return (
+    <button
+      className="border border-indigo-300 bg-indigo-500 py-2 px-5 text-white rounded-md hover:bg-transparent hover:text-indigo-500 transition-all duration-300"
+      onClick={handleToggleMode}
+    >
+      Change Mode to {isDarkMode ? "Light" : "Dark"}
+    </button>
+  );
+};
